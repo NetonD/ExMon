dc434746c438d86c31c6a58ccabf8062387fef57
Almir Neto <almir.neto03@hotmail.com>
2020-08-11 01:18:23 -0300

    Change turn and status round

--- lib/game/actions/attack.ex
+++ lib/game/actions/attack.ex
@@ -1,5 +1,6 @@
 defmodule ExMon.Game.Actions.Attack do
   alias ExMon.Game
+  alias ExMon.Game.Status
   @move_rnd_power 10..35
   @move_str_power 18..26
 
@@ -11,22 +12,29 @@ defmodule ExMon.Game.Actions.Attack do
     |> Game.fetch_player()
     |> Map.get(:life)
     |> calculate_life_afteer_damage(damage)
-    |> update_life_opponent(opponent)
+    |> update_life_opponent(opponent,damage)
   end
 
-  def evaluate_power(:move_random), do: Enum.random(@move_rnd_power)
-  def evaluate_power(:move_strong), do: Enum.random(@move_str_power)
+  defp evaluate_power(:move_random), do: Enum.random(@move_rnd_power)
+  defp evaluate_power(:move_strong), do: Enum.random(@move_str_power)
 
-  def calculate_life_afteer_damage(life, damage) when (life - damage) < 0, do: 0
-  def calculate_life_afteer_damage(life, damage) , do: life - damage
+  defp calculate_life_afteer_damage(life, damage) when (life - damage) < 0, do: 0
+  defp calculate_life_afteer_damage(life, damage) , do: life - damage
 
-  def update_life_opponent(new_life, opponent) do
+  defp update_life_opponent(new_life, opponent,damage) do
     opponent
     |> Game.fetch_player()
     |> Map.put(:life,new_life)
-
+    |> update_game(opponent,damage)
 
   end
 
+  defp update_game(state, opponent,damage) do
+    Game.info()
+    |> Map.put(opponent, state)
+    |> Game.update()
+
+    Status.print_message_move(opponent, :attack, damage)
+  end
 
 end
--- lib/game/status.ex
+++ lib/game/status.ex
@@ -11,4 +11,12 @@ defmodule ExMon.Game.Status do
   def print_wrong_move(move) do
     IO.puts("=============== Invalid movement key. [MOVE] #{move}.")
   end
+
+  def print_message_move(:computer, :attack, damage) do
+    IO.puts("!!============= Player dealing on computer #{damage} damage. =============!!")
+  end
+
+  def print_message_move(:player, :attack, damage) do
+    IO.puts("!!============= Computer dealing on player #{damage} damage. =============!!")
+  end
 end
